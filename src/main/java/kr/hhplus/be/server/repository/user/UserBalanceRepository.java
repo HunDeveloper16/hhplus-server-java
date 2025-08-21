a8f0b9ada7a548b52421ea1bd4ad51a38bb39cc7
shlee <aba7500@naver.com>
2025-08-21 10:19:44 +0900

    feat : 회원 잔액 충전시 비관적 락 적용

--- src/main/java/kr/hhplus/be/server/repository/user/UserBalanceRepository.java
+++ src/main/java/kr/hhplus/be/server/repository/user/UserBalanceRepository.java
@@ -21,6 +21,13 @@ public interface UserBalanceRepository extends JpaRepository<UserBalance, Long> 
         return findByUserId(userId).orElseThrow(() -> new NotFoundException("회원 잔액 정보를 찾을 수 없습니다."));
     }
 
+    /**
+     * 비관적 락(Pessimistic Lock) 적용
+     * 조회 시 DB에서 해당 행 잠금. 타 트랜잭션이 해당 row를 수정하려 하면 커밋 전까지 대기
+     *
+     * @param userId 유저 아이디
+     */
+    @Lock(LockModeType.PESSIMISTIC_WRITE) // 쓰기시에만 락 적용. 읽기시에 락 미적용
     @Query("SELECT ub FROM UserBalance ub JOIN ub.user u WHERE u.userId = :userId")
     Optional<UserBalance> findByUserIdForUpdate(@Param("userId") String userId);
 
--- src/main/java/kr/hhplus/be/server/service/UserService.java
+++ src/main/java/kr/hhplus/be/server/service/UserService.java
@@ -34,12 +34,19 @@ public class UserService {
     /**
      * 회원 잔액을 충전합니다.
      *
+     * [동시성 문제]
+     * 동시에 각각 1000원 500원을 충전하는 경우, 동시 요청으로 인해 아래 1.회원 잔액 조회에서 서로 동일한 잔액을 조회하게 됩니다.
+     * 결과적으로 1000원이 먼저 반영되고, 500원이 반영될 것이기 때문에 결과는 1500원이 아닌 500원이 됩니다.
+     *
+     * [해결]
+     * 따라서 일반적으로 잔액을 충전하는 행위는 데이터의 일관성, 정합성이 중요하다고 생각하여 "비관적 락"을 적용하였습니다.
+     *
      * @param request 충전 요청 정보
      */
-    @Transactional
+    @Transactional // 트랜잭션 보장 및 비관적 락 적용시 필요
     public void chargeUserBalance(UserRequestDto.Charge request){
         log.info(" ======= 1. 회원 잔액을 조회합니다. ======= ");
-        UserBalance userBalance = userBalanceRepository.getValidateUserBalanceByUserIdForUpdate(request.getUserId());
+        UserBalance userBalance = userBalanceRepository.getValidateUserBalanceByUserIdForUpdate(request.getUserId()); // 비관적 락 적용
 
         UserBalanceHistory userBalanceHistory = userBalance.addBalance(request.getAmount()); // dirtyChecking
 
