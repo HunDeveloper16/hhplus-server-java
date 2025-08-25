e9486e038a290978385b8deddbbac8016a520d84
shlee <aba7500@naver.com>
2025-08-25 16:12:12 +0900

    fix : 상품 재고 차감 로직 수정

--- src/main/java/kr/hhplus/be/server/service/ProductService.java
+++ src/main/java/kr/hhplus/be/server/service/ProductService.java
@@ -1,10 +1,10 @@
 package kr.hhplus.be.server.service;
 
+import kr.hhplus.be.server.common.enums.StockHistoryType;
 import kr.hhplus.be.server.common.exception.NotFoundException;
+import kr.hhplus.be.server.dto.common.ProductOrderRequest;
 import kr.hhplus.be.server.dto.common.ProductOrderResult;
-import kr.hhplus.be.server.dto.order.OrderRequestDto;
 import kr.hhplus.be.server.dto.product.ProductResponseDto;
-import kr.hhplus.be.server.model.order.OrderItem;
 import kr.hhplus.be.server.model.product.ProductStock;
 import kr.hhplus.be.server.model.product.ProductStockHistory;
 import kr.hhplus.be.server.repository.product.ProductStockHistoryRepository;
@@ -43,14 +43,14 @@ public class ProductService {
     /**
      * 주문 상품들을 처리합니다 (재고 검증, 차감, 총액 계산)
      *
-     * @param orderProducts 주문 상품 목록
+     * @param orderRequest 주문 상품 목록
      * @return 처리 결과
      */
     @Transactional
-    public ProductOrderResult processOrderProducts(List<OrderRequestDto.OrderProduct> orderProducts) {
+    public ProductOrderResult processOrderProducts(ProductOrderRequest orderRequest) {
         // 주문 요청 상품 아이디 추출
-        List<String> productIds = orderProducts.stream()
-                .map(OrderRequestDto.OrderProduct::getProductId)
+        List<String> productIds = orderRequest.getOrderProductList().stream()
+                .map(ProductOrderRequest.OrderProduct::getProductId)
                 .collect(Collectors.toList());
 
         // 상품 재고 조회
@@ -60,14 +60,19 @@ public class ProductService {
         List<ProductStockHistory> stockHistories = new ArrayList<>();
         List<ProductOrderResult.ProductOrderItem> orderItems = new ArrayList<>();
 
-        for (OrderRequestDto.OrderProduct orderProduct : orderProducts) {
+        for (ProductOrderRequest.OrderProduct orderProduct : orderRequest.getOrderProductList()) {
             ProductStock productStock = findProductStock(productStocks, orderProduct.getProductId());
 
             // 상품 도메인 로직 - 재고 차감 및 검증
             productStock.reduceStockByOrder(orderProduct.getQuantity());
 
             // 히스토리 생성
-            stockHistories.add(ProductStockHistory.ofOrder(productStock, orderProduct.getQuantity()));
+            stockHistories.add(ProductStockHistory.builder()
+                    .productStock(productStock)
+                    .quantity(orderProduct.getQuantity())
+                    .type(StockHistoryType.ORDER)
+                    .orderNo(orderRequest.getOrderNo())
+                    .build());
 
             // 각 아이템별 총액 계산
             totalAmount += productStock.getProduct().getPrice() * orderProduct.getQuantity();
@@ -83,12 +88,14 @@ public class ProductService {
             );
         }
 
+        // 재고 정보 저장
+        productStockRepository.saveAll(productStocks);
+
         // 재고 히스토리 저장
-        saveHistories(stockHistories);
+        productStockHistoryRepository.saveAll(stockHistories);
 
         // 주문 상품 결과 return
         return ProductOrderResult.of(orderItems, totalAmount);
-
     }
 
     @Transactional(readOnly = true)
@@ -98,11 +105,6 @@ public class ProductService {
         if(stocks==null && stocks.isEmpty()) throw new NotFoundException("재고 정보를 찾을 수 없습니다.");
 
         return stocks;
-    }
-
-    @Transactional
-    public void saveHistories(List<ProductStockHistory> histories){
-        productStockHistoryRepository.saveAll(histories);
     }
 
     public ProductStock findProductStock(List<ProductStock> productStocks, String productId){
