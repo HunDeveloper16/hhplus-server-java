3a3a238abd74b232577dcede81c68a39112ce39e
shlee <aba7500@naver.com>
2025-08-29 14:13:01 +0900

    feat : redis,swagger 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,15 @@ dependencies {
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+    // Spring Data Redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // Redisson(분산 락)
+    implementation("org.redisson:redisson-spring-boot-starter:3.24.3")
 }
 
 tasks.withType<Test> {
